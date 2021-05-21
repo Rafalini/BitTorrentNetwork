1cb44d7f73a4bd5f06b843531b9ed68f00ab47ba
Paweł Bęza <pawel.beza99@gmail.com>
2021-05-21 22:27:23 +0200

    add fixme comment

--- tracker/src/TrackerClient.cpp
+++ tracker/src/TrackerClient.cpp
@@ -18,7 +18,7 @@ std::map<std::string, std::set<std::string>> TrackerClient::sendData(const std::
     // get response
     std::string response = readMsg(sock);
 
-    // trim garbage
+    // FIXME trim garbage
     std::size_t found = response.find_last_of('}');
     response = response.substr(0,found + 1);
 
