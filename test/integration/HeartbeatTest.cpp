2ed562cb934ec4cb67828cbe05993d64a41c9f81
Paweł Bęza <pawel.beza99@gmail.com>
2021-05-21 21:55:14 +0200

    test heartbeat

---
+++ test/integration/HeartbeatTest.cpp
@@ -0,0 +1,26 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include "Peer.hpp"
+
+BOOST_AUTO_TEST_SUITE(Heartbeat)
+
+    BOOST_AUTO_TEST_CASE(SendHeartbeat) {
+        Peer peer;
+        auto res = peer.sendHeartbeat("192.168.20.10", 8080);
+
+        std::set<std::string> expectedIPs{"192.168.20.11", "192.168.20.12", "192.168.20.13"};
+        std::set<std::string> expectedFiles{"file1", "file2", "file3"};
+
+        std::cout << "IPs" << std::endl;
+        for (const auto& ip : expectedIPs) {
+            bool exist = res.find(ip) != res.end();
+            BOOST_ASSERT(exist);
+
+            std::set<std::string> peerFiles = res.at(ip);
+            BOOST_ASSERT(expectedFiles == peerFiles);
+        }
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
